c810f077405667d3beb9f46371acbfb6aeafd701
Aki Sasaki <aki@escapewindow.com>
2017-03-15 16:26:08 -0700

    moar archives coverage

--- tests/test_util_archives.py
+++ tests/test_util_archives.py
@@ -5,8 +5,20 @@ import subprocess
 import signtool.util.archives as archives
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+TARBALL = os.path.join(DATA_DIR, "dirtree.tgz")
 
 
+# noumask
+def test_noumask():
+    orig_umask = os.umask(2)
+    try:
+        archives._noumask()
+        assert os.umask(2) == 0
+    finally:
+        os.umask(orig_umask)
+
+
+# bzip2 {{{1
 def test_bzip2(tmpdir):
     fn = "%s/foo" % tmpdir
     open(fn, "w").write("hello")
@@ -18,16 +30,31 @@ def test_bzip2(tmpdir):
     assert b"hello" == open(fn, 'rb').read()
 
 
+# tar {{{1
 def test_tar(tmpdir):
     tmpdir_path = str(tmpdir)
+    assert not os.path.exists(os.path.join(tmpdir_path, "dir2", "foobar"))
     with pytest.raises(Exception):
         archives.unpacktar(__file__, tmpdir_path)
-    archives.unpacktar(
-        os.path.join(DATA_DIR, "dirtree.tgz"),
-        tmpdir_path
-    )
+    archives.unpacktar(TARBALL, tmpdir_path)
     assert os.path.exists(os.path.join(tmpdir_path, "dir2", "foobar"))
     new_tarball = os.path.join(tmpdir_path, "x.tgz")
     archives.packtar(new_tarball, ["dir1_a/file1_a.tar.gz"], os.path.join(tmpdir_path, "dir1"))
     proc = subprocess.Popen(["tar", '-tzf', new_tarball], stdout=subprocess.PIPE)
     assert b"dir1_a/file1_a.tar.gz" == proc.stdout.read().rstrip()
+
+
+# unpackfile {{{1
+@pytest.mark.parametrize('path,raises,expected', ((
+    TARBALL, False, 'dir2/foobar'
+), (
+    __file__, ValueError, None
+)))
+def test_unpackfile(tmpdir, path, raises, expected):
+    tmpdir_path = str(tmpdir)
+    if raises:
+        with pytest.raises(raises):
+            archives.unpackfile(path, tmpdir_path)
+    else:
+        archives.unpackfile(path, tmpdir_path)
+        assert os.path.exists(os.path.join(tmpdir_path, expected))
